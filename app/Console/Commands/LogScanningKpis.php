e3f9bd05eb7db58445f15ed203c05f45a0daa224
dshannon <dshannon@IFSGL-IT3.antrim.ifsgroup.com>
2019-02-12 14:07:49 +0000

    Scanning KPI implementation

--- app/Console/Commands/LogScanningKpis.php
+++ app/Console/Commands/LogScanningKpis.php
@@ -43,17 +43,72 @@ class LogScanningKpis extends Command
         $finishDate = ($this->option('finish-date')) ? Carbon::parse($this->option('start-date')) : Carbon::today();
 
         $this->info('** Logging Scanning KPIs **');
-        
+
         while ($startDate->lessThanOrEqualTo($finishDate)) {
-            
+
             $this->info('Logging KPIs for ' . $startDate);
-            
-            dispatch(new \App\Jobs\LogScanningKpis($startDate));
+
+            $this->logKpis($startDate);
 
             $startDate->addWeekday();
         }
 
         $this->info('Finished');
+    }
+
+    /**
+     * Insert KPI record.
+     * 
+     * @param type $date
+     * @return boolean
+     */
+    protected function logKpis($date)
+    {
+        // Load package records for date specified
+        $packages = \App\Package::select('packages.*')
+                ->join('shipments', 'packages.shipment_id', '=', 'shipments.id')
+                ->whereBetween('ship_date', [Carbon::parse($date)->startOfDay(), Carbon::parse($date)->endOfDay()])
+                ->where('shipments.depot_id', 1)
+                ->whereNotIn('shipments.status_id', [1, 7])
+                ->whereNotIn('shipments.service_id', [7, 18, 20, 39, 44, 45, 48, 50])
+                ->where('sender_postcode', 'LIKE', 'BT%')
+                ->with('shipment')
+                ->get();
+
+        $totals = ['expected' => $packages->count(), 'collection' => 0, 'receipt' => 0, 'route' => 0, 'receipt_missed' => 0, 'route_missed' => 0];
+
+        // Loop through each package and increment relevant counter
+        foreach ($packages as $package) {
+            inc($totals['collection'], $package->collected);
+            inc($totals['receipt'], $package->true_receipt_scan);
+            inc($totals['route'], $package->loaded);
+
+            if ($package->collected || $package->received || $package->loaded) {
+
+                if (!$package->true_receipt_scan) {
+                    inc($totals['receipt_missed'], 1);
+                    $receipt_missed[] = $package->id;
+                }
+
+                if (!$package->loaded) {
+                    inc($totals['route_missed'], 1);
+                    $route_missed[] = $package->id;
+                }
+            }
+        }
+
+        // Insert or update KPI
+        \App\ScanningKpi::firstOrCreate(['date' => $date])->update([
+            'expected' => $totals['expected'],
+            'collection' => $totals['collection'],
+            'collection_percentage' => ($totals['expected'] > 0 && $totals['collection'] > 0) ? round((100 / $totals['expected']) * $totals['collection'], 1) : 0,
+            'receipt' => $totals['receipt'],
+            'receipt_percentage' => ($totals['expected'] > 0 && $totals['receipt'] > 0) ? round((100 / $totals['expected']) * $totals['receipt'], 1) : 0,
+            'route' => $totals['route'],
+            'route_percentage' => ($totals['expected'] > 0 && $totals['route'] > 0) ? round((100 / $totals['expected']) * $totals['route'], 1) : 0,
+            'receipt_missed' => $totals['receipt_missed'],
+            'route_missed' => $totals['route_missed'],
+        ]);
     }
 
 }
