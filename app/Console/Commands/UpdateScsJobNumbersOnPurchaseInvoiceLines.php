b0c6983b844e9a99d54bbe9b2b245949561a15ce
gmcbroom <gmcbroom@IFSGL-IT1.antrim.ifsgroup.com>
2019-06-11 15:38:01 +0100

    Allow use of Shipment table to try to identify the SCS Job Number for the Purchase invoice line

--- app/Console/Commands/UpdateScsJobNumbersOnPurchaseInvoiceLines.php
+++ app/Console/Commands/UpdateScsJobNumbersOnPurchaseInvoiceLines.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Multifreight\JobHdr;
+use App\PurchaseInvoice;
+use App\Shipment;
 use Illuminate\Console\Command;
 
 class UpdateScsJobNumbersOnPurchaseInvoiceLines extends Command
@@ -38,7 +41,7 @@ class UpdateScsJobNumbersOnPurchaseInvoiceLines extends Command
      */
     public function handle()
     {
-        $purchaseInvoices = \App\PurchaseInvoice::whereStatus(0)->get();
+        $purchaseInvoices = PurchaseInvoice::whereStatus(0)->get();
 
         foreach ($purchaseInvoices as $purchaseInvoice) {
 
@@ -53,19 +56,32 @@ class UpdateScsJobNumbersOnPurchaseInvoiceLines extends Command
                         $line->save();
 
                         $this->line('Updated using SCS job number on shipment record');
-                        
+
+                        continue;
+                    }
+
+                    // Check Shipment for SCS job number
+                    $consignment = Shipment::where('carrier_consignment_number',
+                        $line->carrier_tracking_number)->orderBy('id', 'desc')->first();
+                    if ($consignment) {
+                        $line->scs_job_number = $consignment->scs_job_number;
+                        $line->save();
+
+                        $this->line('Updated using Shipment job number');
+
                         continue;
                     }
 
                     // Check job_header for SCS job number
-                    $withHypen = substr($line->carrier_tracking_number, 0, 3) . '-' . substr($line->carrier_tracking_number, 3);
+                    $withHypen = substr($line->carrier_tracking_number, 0, 3).'-'.substr($line->carrier_tracking_number,
+                            3);
 
-                    $jobHdr = \App\Multifreight\JobHdr::select('job_disp')
-                            ->orWhere('hawb_char', $line->carrier_tracking_number)
-                            ->orWhere('hawb_char', $withHypen)
-                            ->orWhere('mawb_char', $line->carrier_tracking_number)
-                            ->orWhere('mawb_char', $withHypen)
-                            ->first();
+                    $jobHdr = JobHdr::select('job_disp')
+                        ->orWhere('hawb_char', $line->carrier_tracking_number)
+                        ->orWhere('hawb_char', $withHypen)
+                        ->orWhere('mawb_char', $line->carrier_tracking_number)
+                        ->orWhere('mawb_char', $withHypen)
+                        ->first();
 
                     if ($jobHdr) {
                         $line->scs_job_number = $jobHdr->job_disp;
