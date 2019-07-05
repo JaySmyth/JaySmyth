714e236a45431b001f1f11ac30705d9bb04e6f73
dshannon <dshannon@antrim.ifsgroup.com>
2019-07-05 10:10:52 +0100

    bug fix on order by

--- resources/views/invoice_runs/show.blade.php
+++ resources/views/invoice_runs/show.blade.php
@@ -4,6 +4,6 @@
 
 <h2>Invoice Run: {{$invoiceRun->id}}</h2>
 
-@include('invoice_runs.partials.shipments', ['shipments' => $invoiceRun->shipments()->orderBy('sender_company_name','consignment_number')->get()])
+@include('invoice_runs.partials.shipments', ['shipments' => $invoiceRun->shipments()->orderBy('sender_company_name')->orderBy('consignment_number')->get()])
 
 @endsection
