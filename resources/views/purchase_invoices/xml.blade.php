44468d9cd2b3c7fdd9bb3709165dbd841a3ad138
dshannon <dshannon@antrim.ifsgroup.com>
2019-07-09 14:22:18 +0100

    xml - display unescaped html

--- resources/views/purchase_invoices/xml.blade.php
+++ resources/views/purchase_invoices/xml.blade.php
@@ -2,18 +2,17 @@
 
 @section('content')
 
-@include('purchase_invoices.partials.title', ['purchaseInvoice'=> $purchaseInvoice, 'small' => 'XML Preview'])
+    @include('purchase_invoices.partials.title', ['purchaseInvoice'=> $purchaseInvoice, 'small' => 'XML Preview'])
 
-<div class="clearfix"></div>
+    <div class="clearfix"></div>
 
-<div class="card">
-    <div class="card-header bg-secondary text-white">
-        Multifreight XML Preview
+    <div class="card">
+        <div class="card-header bg-secondary text-white">
+            Multifreight XML Preview
+        </div>
+        <div class="card-body bg-light">
+            <pre>{!! htmlentities($xml) !!}</pre>
+        </div>
     </div>
-    <div class="card-body bg-light">
-        <pre>{{htmlentities($xml)}}</pre>
-    </div>
-</div>
-
 
 @endsection
