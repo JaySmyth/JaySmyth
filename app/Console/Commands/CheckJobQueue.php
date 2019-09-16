f540ae9efe205e01b8c9aa643b6b92d592fab8d0
dshannon <dshannon@antrim.ifsgroup.com>
2019-09-16 09:08:47 +0100

    bug fix

--- app/Console/Commands/CheckJobQueue.php
+++ app/Console/Commands/CheckJobQueue.php
@@ -53,7 +53,7 @@ class CheckJobQueue extends Command
 
     /**
      * Number of times an alarm has been raised
-     * Starts at zero so first alarm does not 
+     * Starts at zero so first alarm does not
      * send an email
      *
      * @var string
@@ -159,7 +159,8 @@ class CheckJobQueue extends Command
             // Execute bash script to restart job queue
             $resp = exec('/usr/local/bin/restart_job_queue');
             $message .= "Response received:\n\n";
-            if ($resp <> []) {
+
+            if (is_array($resp)) {
                 foreach ($resp as $line) {
                     $message .= $line . "\n";
                 }
