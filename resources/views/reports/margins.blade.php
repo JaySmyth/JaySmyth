84e8fcd3432c75c926585739d19bfde8d12567c3
Daryl Shannon <daryl.shannon@gmail.com>
2019-10-08 16:09:03 +0100

    bug fix to margin report

--- resources/views/reports/margins.blade.php
+++ resources/views/reports/margins.blade.php
@@ -2,58 +2,58 @@
 
 @section('content')
 
-<div class="row">
+    <div class="row">
 
-    <div class="col-sm-2 d-none d-sm-block bg-light sidebar">
+        <div class="col-sm-2 d-none d-sm-block bg-light sidebar">
 
-        {!! Form::Open(['url' => Request::path(), 'method' => 'get', 'autocomplete' => 'off']) !!}
+            {!! Form::Open(['url' => Request::path(), 'method' => 'get', 'autocomplete' => 'off']) !!}
 
-        <div class="form-group">
-            <label for="date_from">Date From</label>
-            <input type="text" name="date_from" value="@if(Auth::user()->hasIfsRole() && !Input::get('date_from') && !Input::get('date_to')){{date(Auth::user()->date_format)}}@else{{Input::get('date_from')}}@endif" class="form-control datepicker" placeholder="Date from">
+            <div class="form-group">
+                <label for="date_from">Date From</label>
+                <input type="text" name="date_from" value="@if(Auth::user()->hasIfsRole() && !Input::get('date_from') && !Input::get('date_to')){{date(Auth::user()->date_format)}}@else{{Input::get('date_from')}}@endif" class="form-control datepicker" placeholder="Date from">
+            </div>
+            <div class="form-group">
+                <label for="date_to">Date To</label>
+                <input type="text" name="date_to" value="@if(Auth::user()->hasIfsRole() && !Input::get('date_from') && !Input::get('date_to')){{date(Auth::user()->date_format)}}@else{{Input::get('date_to')}}@endif" class="form-control datepicker" placeholder="Date To">
+            </div>
+
+            <div class="form-group">
+                <label for="month">Traffic</label>
+                {!! Form::select('traffic', dropDown('traffic', 'All Traffic'), Input::get('traffic'), array('class' => 'form-control')) !!}
+            </div>
+
+            <div class="form-group">
+                <label for="month">Carrier</label>
+                {!! Form::select('carrier', dropDown('carriers', 'All Carriers'), Input::get('carrier'), array('class' => 'form-control')) !!}
+            </div>
+
+            <div class="form-group">
+                <label for="month">Depot</label>
+                {!! Form::select('depot', dropDown('associatedDepots', 'All Depots'), Input::get('depot'), array('class' => 'form-control')) !!}
+            </div>
+
+            <div class="form-group">
+                <label for="month">Service</label>
+                {!! Form::select('service', dropDown('services', 'All Services'), Input::get('service'), array('class' => 'form-control')) !!}
+            </div>
+
+            <div class="form-group">
+                <label for="month">Shipper</label>
+                {!! Form::select('company', dropDown('enabledSites', 'All Shippers'), Input::get('company'), array('class' => 'form-control')) !!}
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Update Report</button>
+
+            {!! Form::Close() !!}
+
         </div>
-        <div class="form-group">
-            <label for="date_to">Date To</label>
-            <input type="text" name="date_to" value="@if(Auth::user()->hasIfsRole() && !Input::get('date_from') && !Input::get('date_to')){{date(Auth::user()->date_format)}}@else{{Input::get('date_to')}}@endif" class="form-control datepicker" placeholder="Date To">
-        </div>
 
-        <div class="form-group">
-            <label for="month">Traffic</label>
-            {!! Form::select('traffic', dropDown('traffic', 'All Traffic'), Input::get('traffic'), array('class' => 'form-control')) !!}
-        </div>
+        <main class="col-sm-10 ml-sm-auto" role="main">
 
-        <div class="form-group">
-            <label for="month">Carrier</label>
-            {!! Form::select('carrier', dropDown('carriers', 'All Carriers'), Input::get('carrier'), array('class' => 'form-control')) !!}
-        </div>
+            @include('partials.title', ['title' => $report->name, 'results'=> $shipments])
 
-        <div class="form-group">
-            <label for="month">Depot</label>
-            {!! Form::select('depot', dropDown('associatedDepots', 'All Depots'), Input::get('depot'), array('class' => 'form-control')) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="month">Service</label>
-            {!! Form::select('service', dropDown('services', 'All Services'), Input::get('service'), array('class' => 'form-control')) !!}
-        </div>
-
-        <div class="form-group">
-            <label for="month">Shipper</label>
-            {!! Form::select('company', dropDown('enabledSites', 'All Shippers'), Input::get('company'), array('class' => 'form-control')) !!}
-        </div>
-
-        <button type="submit" class="btn btn-primary mt-3">Update Report</button>
-
-        {!! Form::Close() !!}
-
-    </div>
-
-    <main class="col-sm-10 ml-sm-auto" role="main">
-
-        @include('partials.title', ['title' => $report->name, 'results'=> $shipments])
-
-        <table class="table table-striped table-bordered table-sm">
-            <thead>
+            <table class="table table-striped table-bordered table-sm">
+                <thead>
                 <tr class="success">
                     <th>Consignment</th>
                     <th>Service</th>
@@ -68,75 +68,86 @@
                     <th class="text-right">Profit/Loss</th>
                     <th class="text-right">Margin</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 @foreach($shipments as $shipment)
 
-                @if(stristr($shipment->margin_styling_class, 'danger'))
-                <tr class="text-nowrap table-danger">
+                    @if(stristr($shipment->margin_styling_class, 'danger'))
+                        <tr class="text-nowrap table-danger">
                     @else
-                <tr class="text-nowrap">
-                    @endif
+                        <tr class="text-nowrap">
+                            @endif
 
-                    <td><a href="{{ url('/shipments', $shipment->id) }}" class="consignment-number">{{$shipment->carrier_consignment_number}}</a></td>
-                    <td class="text-truncate">{{$shipment->service->carrier_name}}</td>
-                    <td class="text-truncate"><a href="{{ url('/companies', $shipment->company->id) }}">{{$shipment->company->company_name}}</a></td>
-                    <td class="text-uppercase">
-                        @if($shipment->bill_shipping_account)
-                        {{$shipment->bill_shipping_account}}
-                        @else
-                        <span class="text-muted">null</span>
-                        @endif
-                    </td>
-                    <td class="align-middle text-right">{{$shipment->pieces}}</td>
-                    <td class="text-right">{{$shipment->chargeable_weight}} {{$shipment->weight_uom}}</td>
-                    <td class="text-center text-uppercase">
-                        @if(is_array($shipment->quoted_array) && isset($shipment->quoted_array['costs_zone']))
+                            <td>
+                                <a href="{{ url('/shipments', $shipment->id) }}" class="consignment-number">{{$shipment->carrier_consignment_number}}</a>
+                            </td>
+                            <td class="text-truncate">{{$shipment->service->carrier_name}}</td>
+                            <td class="text-truncate">
+                                <a href="{{ url('/companies', $shipment->company->id) }}">{{$shipment->company->company_name}}</a>
+                            </td>
+                            <td class="text-uppercase">
+                                @if($shipment->bill_shipping_account)
+                                    {{$shipment->bill_shipping_account}}
+                                @else
+                                    <span class="text-muted">null</span>
+                                @endif
+                            </td>
+                            <td class="align-middle text-right">{{$shipment->pieces}}</td>
+                            <td class="text-right">{{$shipment->chargeable_weight}} {{$shipment->weight_uom}}</td>
+                            <td class="text-center text-uppercase">
+                                @if(is_array($shipment->quoted_array) && isset($shipment->quoted_array['costs_zone']))
 
-                        @if(isset($shipment->quoted_array['costs_model']))
-                        {{$shipment->quoted_array['costs_model']}}
-                        @endif
+                                    @if(isset($shipment->quoted_array['costs_model']))
+                                        {{$shipment->quoted_array['costs_model']}}
+                                    @endif
 
-                        {{$shipment->quoted_array['costs_zone']}}
+                                    {{$shipment->quoted_array['costs_zone']}}
 
-                        @else
-                        <span class="text-muted">null</span>
-                        @endif
+                                @else
+                                    <span class="text-muted">null</span>
+                                @endif
 
-                    </td>
+                            </td>
 
-                    <td class="text-center text-uppercase">
-                        @if(is_array($shipment->quoted_array) && isset($shipment->quoted_array['sales_zone']))
-                        @if(isset($shipment->quoted_array['sales_model']))
-                        {{$shipment->quoted_array['sales_model']}}
-                        @endif
+                            <td class="text-center text-uppercase">
+                                @if(is_array($shipment->quoted_array) && isset($shipment->quoted_array['sales_zone']))
+                                    @if(isset($shipment->quoted_array['sales_model']))
+                                        {{$shipment->quoted_array['sales_model']}}
+                                    @endif
 
-                        {{$shipment->quoted_array['sales_zone']}}
-                        @else
-                        <span class="text-muted">null</span>
-                        @endif
-                    </td>
+                                    {{$shipment->quoted_array['sales_zone']}}
+                                @else
+                                    <span class="text-muted">null</span>
+                                @endif
+                            </td>
 
-                    <td class="text-right" title="Cost">{{number_format($shipment->shipping_cost, 2)}}</td>
-                    <td class="text-right" title="Sales">{{number_format($shipment->shipping_charge, 2)}}</td>
-                    <td class="text-right" title="Profit/Loss"><span class="{{$shipment->margin_styling_class}}">{{$shipment->profit_formatted}}</span></td>
-                    <td class="text-right" title="Margin"><span class="{{$shipment->margin_styling_class}}">{{$shipment->margin}}</span></td>
-                </tr>
-                @endforeach
-                    <tr class="font-weight-bold">
-                        <td colspan="8" class="text-muted text-right">Totals for this page:</td>
-                        <td class="text-right">{{ number_format($shipments->sum('shipping_cost'), 2) }}</td>
-                        <td class="text-right">{{ number_format($shipments->sum('shipping_charge'), 2) }}</td>
-                        <td class="text-right">{{ number_format($shipments->sum('profit'), 2) }}</td>
-                        <td class="text-right">{{ number_format(($shipments->sum('shipping_charge') - $shipments->sum('shipping_cost')) / $shipments->sum('shipping_charge') * 100, 2) }}%</td>
-                    </tr>
-            </tbody>
-        </table>
+                            <td class="text-right" title="Cost">{{number_format($shipment->shipping_cost, 2)}}</td>
+                            <td class="text-right" title="Sales">{{number_format($shipment->shipping_charge, 2)}}</td>
+                            <td class="text-right" title="Profit/Loss">
+                                <span class="{{$shipment->margin_styling_class}}">{{$shipment->profit_formatted}}</span>
+                            </td>
+                            <td class="text-right" title="Margin">
+                                <span class="{{$shipment->margin_styling_class}}">{{$shipment->margin}}</span></td>
+                        </tr>
+                        @endforeach
+                        <tr class="font-weight-bold">
+                            <td colspan="8" class="text-muted text-right">Totals for this page:</td>
+                            <td class="text-right">{{ number_format($shipments->sum('shipping_cost'), 2) }}</td>
+                            <td class="text-right">{{ number_format($shipments->sum('shipping_charge'), 2) }}</td>
+                            <td class="text-right">{{ number_format($shipments->sum('profit'), 2) }}</td>
+                            @if($shipments->sum('shipping_charge') > 0)
+                                <td class="text-right">{{ number_format(($shipments->sum('shipping_charge') - $shipments->sum('shipping_cost')) / $shipments->sum('shipping_charge') * 100, 2) }}%</td>
+                            @else
+                                <td class="text-right">0.00</td>
+                            @endif
+                        </tr>
+                </tbody>
+            </table>
 
-        @include('partials.no_results', ['title' => 'shippers', 'results'=> $shipments])
-        @include('partials.pagination', ['results'=> $shipments])
+            @include('partials.no_results', ['title' => 'shippers', 'results'=> $shipments])
+            @include('partials.pagination', ['results'=> $shipments])
 
-    </main>
-</div>
+        </main>
+    </div>
 
 @endsection
