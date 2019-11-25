9bd7d84c3fcbb9c9226b5a9d255e601e9e1abe0b
dshannon <dshannon@antrim.ifsgroup.com>
2019-11-25 11:02:29 +0000

    re run sales invoice export

--- app/Http/Controllers/ShipmentsController.php
+++ app/Http/Controllers/ShipmentsController.php
@@ -929,12 +929,9 @@ class ShipmentsController extends Controller
     public function test(Request $request)
     {
 
-        $trackingNumber = '47312482';
+        $shipments = Shipment::where('invoice_run_id', 783)->pluck('id');
 
-        $trackingNumber = str_pad($trackingNumber, 8, 0, STR_PAD_LEFT);
-        $trackingNumber .= mod11CheckDigit((string)$trackingNumber);                        // Then add check digit
-
-        dd($trackingNumber);
+        dispatch(new \App\Jobs\GenerateMultifreightSalesXml($shipments, 0, 2880));
 
     }
 
