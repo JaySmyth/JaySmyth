e958bc882e7c913b9d3873b36e775ed378e6b543
dshannon <dshannon@antrim.ifsgroup.com>
2019-11-27 14:07:56 +0000

    DHL - round values to 2 decimal places

--- app/CarrierAPI/DHL/DHL.php
+++ app/CarrierAPI/DHL/DHL.php
@@ -431,7 +431,7 @@ class DHL
                         "Quantity" => $content['quantity'],
                         "QuantityUnitOfMeasurement" => $content['uom'],
                         "ItemDescription" => substr(trim($content['description']), 0, 35),
-                        "UnitPrice" => $content['unit_value'],
+                        "UnitPrice" => round($content['unit_value'], 2),
                         "NetWeight" => round($content['unit_weight'] * $content['quantity'], 2),
                         "GrossWeight" => round($content['unit_weight'] * $content['quantity'], 2),
                         "ManufacturingCountryCode" => $content['country_of_manufacture']
