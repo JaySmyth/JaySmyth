0efc44cd2b30bafd1f53aa9d0f5a8d810aeee623
dshannon <dshannon@antrim.ifsgroup.com>
2019-11-29 15:28:28 +0000

    DN env changes

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => false,
             'engine' => null,
             'options' => array(
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_AWS_CA_PEM', null)
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_AWS_CA_PEM_TEMP', null)
             )
         ],
 
@@ -76,7 +76,7 @@ return [
             'strict' => false,
             'engine' => null,
             'options' => array(
-                PDO::MYSQL_ATTR_SSL_CA => env('DB_AWS_CA_PEM', null)
+                PDO::MYSQL_ATTR_SSL_CA => env('DB_AWS_CA_PEM_TEMP', null)
             )
         ],
 
