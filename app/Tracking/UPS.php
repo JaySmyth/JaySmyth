30b568caf74142dafc6d5ec6dbfbba6c11158a2f
dshannon <dshannon@antrim.ifsgroup.com>
2019-12-11 16:04:06 +0000

    tracking debug

--- app/Tracking/UPS.php
+++ app/Tracking/UPS.php
@@ -102,6 +102,9 @@ class UPS extends Tracking
         //$response = Arr::dot($response);
         //dd($response);
 
+        $debug = Arr::dot($response);
+        Mail::to('dshannon@antrim.ifsgroup.com')->send(new \App\Mail\GenericError('Get UPS tracking ' . $this->trackingNumber . ' - debug', $debug));
+
         // Error encountered
         if (isset($response['Fault'])) {
             $response = Arr::dot($response);
