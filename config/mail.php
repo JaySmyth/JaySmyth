6d9c0aa2a7757555e2fd575df6948a7ba4a4fe99
dshannon <dshannon@antrim.ifsgroup.com>
2020-02-14 14:16:47 +0000

    change mail setting

--- config/mail.php
+++ config/mail.php
@@ -56,9 +56,12 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@antrim.ifsgroup.com'),
+        'name' => env('MAIL_FROM_NAME', 'noreply@antrim.ifsgroup.com'),
     ],
+
+
+    /*
 
     /*
     |--------------------------------------------------------------------------
