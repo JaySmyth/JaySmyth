4be4a9e82bb1d9017481a268b85bddb86668677d
dshannon <dshannon@antrim.ifsgroup.com>
2020-02-25 12:03:20 +0000

    fixed scope bug in ManifestProfile.php

--- app/ShipmentScopes.php
+++ app/ShipmentScopes.php
@@ -555,7 +555,7 @@ trait ShipmentScopes
     public function scopeIsNotFedexCollect($query)
     {
         return $query->where('bill_shipping', '!=', 'recipient')
-            ->where('bill_shipping_account', 205691588)
+            //->where('bill_shipping_account', 205691588)
             ->where('carrier_id', 2)
             ->whereIn('sender_country_code', getUkDomesticCountries())
             ->whereNotIn('recipient_country_code', getUkDomesticCountries());
