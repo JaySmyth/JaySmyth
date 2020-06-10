b9b3580e11987644a048bb85744c331e3007e479
dshannon <dshannon@antrim.ifsgroup.com>
2020-06-10 09:08:17 +0100

    validation rules - AddressRequest.php

--- app/Http/Requests/AddressRequest.php
+++ app/Http/Requests/AddressRequest.php
@@ -25,16 +25,16 @@ class AddressRequest extends Request
     {
         return [
             'name' => 'required|min:2|max:35',
-            'company_name' => 'min:2|max:35',
+            'company_name' => 'nullable|min:2|max:35',
             'address1' => 'required|min:2|max:35',
-            'address2' => 'max:35',
-            'address3' => 'max:35',
+            'address2' => 'nullable|max:35',
+            'address3' => 'nullable|max:35',
             'city' => 'required|regex:/^[a-zA-Z \'-]+$/|min:2|max:35',
-            'state' => 'regex:/^[a-zA-Z ]+$/|min:2|max:35',
+            'state' => 'nullable|regex:/^[a-zA-Z ]+$/|min:2|max:35',
             'postcode' => 'min:2|max:10',
             'country_code' => 'required|alpha|size:2',
             'telephone' => 'required|min:3|max:20',
-            'email' => 'email|max:50',
+            'email' => 'nullable|email|max:50',
             'type' => 'required|alpha|size:1',
             'definition' => 'alpha|min:6|max:9',
         ];
