5337c3f4492bcafea8569c87271f1de6a50c9663
dshannon <dshannon@antrim.ifsgroup.com>
2021-01-07 11:51:35 +0000

    Cancel failed reset shipments

--- app/CarrierAPI/ServiceRules.php
+++ app/CarrierAPI/ServiceRules.php
@@ -247,10 +247,10 @@ class ServiceRules
         // Patch to disable specific services between dates (Inclusive)
         $today = date('Y-m-d');
         $heldService = [
-            '22' => ['startHold' => '2020-12-21', 'endHold' => '2021-01-07'],
-            '24' => ['startHold' => '2020-12-21', 'endHold' => '2021-01-07'],
-            '26' => ['startHold' => '2021-01-01', 'endHold' => '2021-01-07'],   // Release after DHL Service upgrade
-            '57' => ['startHold' => '2021-01-01', 'endHold' => '2021-01-07'],   // Release after DHL Service upgrade
+         //   '22' => ['startHold' => '2020-12-21', 'endHold' => '2021-01-07'],
+         //   '24' => ['startHold' => '2020-12-21', 'endHold' => '2021-01-07'],
+         //   '26' => ['startHold' => '2021-01-01', 'endHold' => '2021-01-07'],   // Release after DHL Service upgrade
+         //   '57' => ['startHold' => '2021-01-01', 'endHold' => '2021-01-07'],   // Release after DHL Service upgrade
         ];
         if (isset($heldService[$serviceDetails['id']])) {
             if ($today >= $heldService[$serviceDetails['id']]['startHold'] && $today <= $heldService[$serviceDetails['id']]['endHold']) {
