e2ded954ac17e853b7b1d39a5abcfb67991237f5
dshannon <dshannon@antrim.ifsgroup.com>
2021-02-12 14:23:43 +0000

    add ref to label

--- app/CarrierAPI/ExpressFreight/ExpressFreightLabel.php
+++ app/CarrierAPI/ExpressFreight/ExpressFreightLabel.php
@@ -169,9 +169,11 @@ class ExpressFreightLabel extends \App\CarrierAPI\CarrierLabel
             //$this->pdf->Text($x, $y, 'Fax - 028 38323005');
 
             $x = 7;
-            $y = 141;
-            $this->pdf->SetFont($this->font, 'B', 14);
+            $y = 138;
+            $this->pdf->SetFont($this->font, 'B', 12);
             $this->pdf->Text($x, $y, 'IFS CONSIGNMENT#: '.$this->data['ifs_consignment_number']);
+            $this->pdf->SetFont($this->font, 'B', 10);
+            $this->pdf->Text($x, $y+5, 'SHIPPER REF: '.$this->data['shipment_reference']);
             $pkg++;
         }
 
